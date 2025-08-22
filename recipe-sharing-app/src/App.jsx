44214c11327fb9ceab4a19d5b8532f84b28bafe4
Ayaa-boy <prattnanak02@gmail.com.com>
2025-08-22 14:56:18 +0000

    Enhance recipe-sharing-app with recipe details, edit and delete functionality using Zustand and React Router

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,15 +1,27 @@
-// src/App.jsx
-import AddRecipeForm from "./components/AddRecipeForm";
+import { BrowserRouter as Router, Routes, Route, Link, useParams } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
+import { RecipeDetails } from "./components/RecipeDetails";
 
 function App() {
   return (
-    <div>
-      <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+    <Router>
+      <div>
+        <h1>Recipe Sharing App</h1>
+        <AddRecipeForm />
+        <RecipeList />
+
+        <Routes>
+          <Route path="/recipe/:id" element={<RecipePage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
+const RecipePage = () => {
+  const { id } = useParams();
+  return <RecipeDetails recipeId={id} />;
+};
 
 export default App;
