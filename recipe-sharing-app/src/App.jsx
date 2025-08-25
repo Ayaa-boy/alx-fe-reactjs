4aa2f5ea5cc6032b8bf97c6e692db20c327a483a
Ayaa-boy <prattnanak02@gmail.com.com>
2025-08-25 21:44:03 +0000

    fix: add AddRecipeForm route to App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,7 +1,8 @@
+// src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
 import RecipeDetails from "./components/RecipeDetails";
-import EditRecipeForm from "./components/EditRecipeForm";
+import AddRecipeForm from "./components/AddRecipeForm";  // ✅ Import added
 import FavoritesList from "./components/FavoritesList";
 import RecommendationsList from "./components/RecommendationsList";
 
@@ -10,8 +11,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<RecipeList />} />
-        <Route path="/recipes/:id" element={<RecipeDetails />} />
-        <Route path="/edit/:id" element={<EditRecipeForm />} />
+        <Route path="/recipe/:id" element={<RecipeDetails />} />
+        <Route path="/add" element={<AddRecipeForm />} /> {/* ✅ Route added */}
         <Route path="/favorites" element={<FavoritesList />} />
         <Route path="/recommendations" element={<RecommendationsList />} />
       </Routes>
