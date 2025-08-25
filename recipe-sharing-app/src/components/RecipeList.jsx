a57122d7843c3bb85517652262e669e903d9099a
Ayaa-boy <prattnanak02@gmail.com.com>
2025-08-25 15:01:05 +0000

    ALX: Fix RecipeList to use useRecipeStore, add recipeStore.js, integrate SearchBar and RecipeDetails

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,14 +1,22 @@
 // src/components/RecipeList.jsx
 import { Link } from "react-router-dom";
+import { useRecipeStore } from "./recipeStore";
 
-export default function RecipeList({ recipes }) {
+export default function RecipeList() {
+  const searchTerm = useRecipeStore((state) => state.searchTerm);
+  const recipes = useRecipeStore((state) => state.recipes);
+  const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
+
+  const displayRecipes = searchTerm ? filteredRecipes : recipes;
+
   return (
     <ul>
-      {recipes.map((r) => (
+      {displayRecipes.map((r) => (
         <li key={r.id}>
           <Link to={`/recipe/${r.id}`}>{r.title}</Link>
         </li>
       ))}
+      {displayRecipes.length === 0 && <p>No recipes found.</p>}
     </ul>
   );
 }
